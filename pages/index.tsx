848efb0c9a690db674a987b6f19de81da30da1bb
Jonathan Trang <jon301@gmail.com>
2021-11-22 15:28:07 +0100

    Move NextJS pages component to a dedicated folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,3 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import React from 'react';
-import { WebcamRecorder } from '../components/WebcamRecorder/WebcamRecorder';
-
-const ProgressiveUploadDemoPage: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Progressive Upload Demo</title>
-        <meta name="description" content="Progressive Upload Demo" />
-        <link
-          rel="icon"
-          sizes="16x16"
-          href="https://api-video.imgix.net/1631544061-favicon-32x32.png?w=16&amp;h=16"
-          type="image/png"
-        />
-      </Head>
-
-      <main>
-        <WebcamRecorder />
-      </main>
-    </div>
-  );
-};
+import { ProgressiveUploadDemoPage } from '../screens/ProgressiveUploadDemoPage';
 
 export default ProgressiveUploadDemoPage;
